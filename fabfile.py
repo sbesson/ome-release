5d11b62cec2ad8be876e2e01f1035ce3ade9a815
jmoore <josh@glencoesoftware.com>
2013-10-08 22:35:06 +0200

    Two-step gen.py & pelican

--- fabfile.py
+++ fabfile.py
@@ -15,11 +15,25 @@ env.cloudfiles_username = 'my_rackspace_username'
 env.cloudfiles_api_key = 'my_rackspace_api_key'
 env.cloudfiles_container = 'my_cloudfiles_container'
 
-
 def clean():
     if os.path.isdir(DEPLOY_PATH):
         local('rm -rf {deploy_path}'.format(**env))
-        local('mkdir {deploy_path}'.format(**env))
+        local('mkdir -p {deploy_path}'.format(**env))
+    if os.path.isdir('content'):
+        local('rm -rf {deploy_path}'.format(**env))
+        local('mkdir -p {deploy_path}'.format(**env))
+
+def omero(tag, build):
+    clean()
+    local("mkdir -p content")
+    local('python gen.py %s %s > content/index.md' % (tag, build))
+    build()
+
+def bf(tag, build):
+    clean()
+    local("mkdir -p content")
+    local('python gen.py %s %s > content/index.md' % (tag, build))
+    build()
 
 def build():
     local('pelican -s pelicanconf.py content')
@@ -37,24 +51,3 @@ def serve():
 def reserve():
     build()
     serve()
-
-def preview():
-    local('pelican -s publishconf.py')
-
-def cf_upload():
-    rebuild()
-    local('cd {deploy_path} && '
-          'swift -v -A https://auth.api.rackspacecloud.com/v1.0 '
-          '-U {cloudfiles_username} '
-          '-K {cloudfiles_api_key} '
-          'upload -c {cloudfiles_container} .'.format(**env))
-
-@hosts(production)
-def publish():
-    local('pelican -s publishconf.py')
-    project.rsync_project(
-        remote_dir=dest_path,
-        exclude=".DS_Store",
-        local_dir=DEPLOY_PATH.rstrip('/') + '/',
-        delete=True
-    )
