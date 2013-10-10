d1cb502a577b4a8d50f20bed44b7c9e6a1987329
Sebastien Besson <seb.besson@gmail.com>
2013-10-10 12:39:10 +0100

    Skip tag SHA1 retrieval until used in the Bio-Formats landing page

--- bfgen.py
+++ bfgen.py
@@ -29,23 +29,23 @@ import re
 split_version =  re.split("^([0-9]+)\.([0-9]+)\.([0-9]+)(.*?)$", version)
 major_version = int(split_version[1])
 
-# Creating Github instance
-try:
-    p = subprocess.Popen("git","config","--get","github.token", stdout = subprocess.PIPE)
-    rc = p.wait()
-    if rc:
-        raise Exception("rc=%s" % rc)
-    token = p.communicate()
-except Exception:
-    token = None
-
-gh = github.Github(token, user_agent="PyGithub")
-repo = gh.get_organization("openmicroscopy").get_repo("bioformats")
-for tag in repo.get_tags():
-    if tag.name == ("v%s" % version):
-        break
-repl["@SHA1_FULL@"] = tag.commit.sha
-repl["@SHA1_SHORT@"] = tag.commit.sha[0:10]
+# # Creating Github instance
+# try:
+#     p = subprocess.Popen("git","config","--get","github.token", stdout = subprocess.PIPE)
+#     rc = p.wait()
+#     if rc:
+#         raise Exception("rc=%s" % rc)
+#     token = p.communicate()
+# except Exception:
+#     token = None
+#
+# gh = github.Github(token, user_agent="PyGithub")
+# repo = gh.get_organization("openmicroscopy").get_repo("bioformats")
+# for tag in repo.get_tags():
+#     if tag.name == ("v%s" % version):
+#         break
+# repl["@SHA1_FULL@"] = tag.commit.sha
+# repl["@SHA1_SHORT@"] = tag.commit.sha[0:10]
 repl["@DOC_URL@"] = "https://www.openmicroscopy.org/site/support/bio-formats%s" % major_version
 if "STAGING" in os.environ and os.environ.get("STAGING"):
     repl["@DOC_URL@"] += "-staging"
