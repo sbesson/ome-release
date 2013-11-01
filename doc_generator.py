3eab324d71f91c8700e4aa8b29a5d2fbb0fb74f5
Sebastien Besson <seb.besson@gmail.com>
2013-11-01 14:25:01 +0000

    Fix human size generation (using powers of 10 instead of 1024)

--- doc_generator.py
+++ doc_generator.py
@@ -118,8 +118,8 @@ def humansize(nbytes):
     if nbytes == 0:
         return '0 B'
     i = 0
-    while nbytes >= 1024 and i < len(suffixes)-1:
-        nbytes /= 1024.
+    while nbytes >= 1000 and i < len(suffixes)-1:
+        nbytes /= 1000.
         i += 1
     f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
     return '%s %s' % (f, suffixes[i])
