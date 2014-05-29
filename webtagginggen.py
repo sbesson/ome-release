28580415186a083acc81c1bab5ab3e43aea81a3f
Sebastien Besson <seb.besson@gmail.com>
2014-05-29 08:41:31 +0100

    Fix typo in ANNOUNCEMENT_URL environment variable

--- webtagginggen.py
+++ webtagginggen.py
@@ -30,7 +30,7 @@ PREFIX = os.environ.get('PREFIX', 'webtagging')
 WEBTAGGING_RSYNC_PATH = '%s/%s/%s/' % (RSYNC_PATH, PREFIX, version)
 
 forum_url = "https://www.openmicroscopy.org/community/viewforum.php?f=11"
-repl["@ANNOUCEMENT_URL@"] = os.environ.get('ANNOUCEMENT_URL', forum_url)
+repl["@ANNOUNCEMENT_URL@"] = os.environ.get('ANNOUNCEMENT_URL', forum_url)
 
 for x, y in (
         ("WEBTAGGING", "webtagging-@VERSION@.zip"),
