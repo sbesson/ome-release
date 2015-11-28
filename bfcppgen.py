8018248f5ec977dbca47a35e435b5bf46c43f938
Roger Leigh <rleigh@codelibre.net>
2015-11-28 16:18:31 +0000

    bfcppgen: FreeBSD is amd64 rather than x86_64

--- bfcppgen.py
+++ bfcppgen.py
@@ -48,7 +48,7 @@ platforms = {'UBUNTU1404': 'Ubuntu14.04-x86_64',
              'OSX109':     'MacOSX10.9',
              'CENTOS66':   'CentOS6.6-x86_64',
              'CENTOS71':   'CentOS7.1-x86_64',
-             'FREEBSD102': 'FreeBSD10.2-x86_64'}
+             'FREEBSD102': 'FreeBSD10.2-amd64'}
 
 win_platforms = {'WINDOWSVC12X64': 'WindowsVC12-x64',
                  'WINDOWSVC12X86': 'WindowsVC12-x86'}
